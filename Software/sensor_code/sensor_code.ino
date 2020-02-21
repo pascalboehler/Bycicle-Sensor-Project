43902c7fc17470e4b4f3fd45b51b73066f2d5f9f
nucleus-ffm <nico-git@posteo.de>
2020-02-21 13:59:28 +0100

    fixed serial monitor and calculation

--- Software/sensor_code/sensor_code.ino
+++ Software/sensor_code/sensor_code.ino
@@ -1,42 +1,46 @@
 /*
-  Created by Pascal Böhler
+  Created by BSP
   Date created: 16.01.2020
+  last editing: 21.02.2020 
 */
 
-int piezoPin = 8;
-// Sensor HC SR04
-int echo = 5;
-int trigger = 6;
-// led
-int led = 11;
+int piezoPin = 8; // Sensor HC SR04
+const int trigger = 6; 
+const int echo = 5;
+
+int led = 11; // led
+
 // vars
 long duration = 0;
-long distance = 0;
+int distance = 0;
 
 void setup() {
   // Piezo Setup:
   pinMode(piezoPin, OUTPUT);
-  // HC Sr04 Setup:
-  Serial.begin(9600);
-  pinMode(trigger, OUTPUT);
+  pinMode(trigger, OUTPUT);// HC Sr04 Setup:
   pinMode(echo, INPUT);
-  // LED
-  pinMode(led, OUTPUT);
+  Serial.begin(9600); //starts the serial communication
+  pinMode(led, OUTPUT); // LED
 }
 
 void loop() {
   digitalWrite(trigger, LOW); // nullen
-  delay(5); // wait 5 millisecs
+  delay(2); // wait 5 millisecs
+  
   digitalWrite(trigger, HIGH); // send "tone"
-  delay(10); // wait for 10 secs (tone plays)
+  delay(10); // wait for 10 millisecs (tone plays)
   digitalWrite(trigger, LOW); // stop tone
+  
   duration = pulseIn(echo, HIGH); // check how long needs to come back
-  distance = (duration/2) * 0.03432; // calc distance (duration / 2) * aircoupling speed
+  distance = duration*0.03432/2; // calc distance (duration / 2) * aircoupling speed
   if (distance >= 500 || distance <= 0) {
     Serial.println("Error no values found!"); // debug message
   }
   else {
-    Serial.println(distance + " cm"); // debug message
+    // debug message
+    Serial.print("Distance: ");
+    Serial.print(distance);
+    Serial.println(" cm"); 
     playSoundAndLight(distance);
   }
   //delay(10);
