107608e29ca5ae9c722ebe750cde49c3e61d3100
Pascal Böhler <pascal.boehler@hotmail.de>
2020-02-23 18:10:05 +0100

    Fixed error in test folder

--- app/digiroad/test/widget_test.dart
+++ app/digiroad/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:digiroad/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(HomeView());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
