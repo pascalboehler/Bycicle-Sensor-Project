ad0e89ca28a6f88ff9f4226fd0db7099062a1466
nucleus-ffm <nico-git@posteo.de>
2020-03-29 14:09:51 +0200

    added BottomNavigationBar

--- app/digiroad/lib/HomeView.dart
+++ app/digiroad/lib/HomeView.dart
@@ -7,6 +7,8 @@ import 'package:scoped_model/scoped_model.dart';
 import './BackgroundCollectingTask.dart';
 import './SelectBondedDevicePage.dart';
 import './MeasureDataPage.dart';
+import './BluetoothData.dart';
+
 
 class HomeView extends StatefulWidget {
   @override
@@ -14,131 +16,65 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-  BluetoothState _bluetoothState = BluetoothState.UNKNOWN;
+  int _selectedIndex = 0;
 
-  String _address = "...";
-  String _name = "...";
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Karte',
+      style: optionStyle,
+    ),
+    BluetoothData(),
+    Text(
+      'Index 3: Settings',
+      style: optionStyle,
+    ),
+  ];
 
-  Timer _discoverableTimeoutTimer;
-  int _discoverableTimeoutSecondsLeft = 0;
-
-  BackgroundCollectingTask _collectingTask;
-
-  bool _autoAcceptPairingRequest = false;
-
-  @override
-  void initState() {
-    super.initState();
-
-    // get current bluetooth state
-    FlutterBluetoothSerial.instance.state.then((state) {
-      setState(() {
-        _bluetoothState = state;
-      });
-    });
-
-    Future.doWhile(() async {
-      if (await FlutterBluetoothSerial.instance.isEnabled) {
-        return false;
-      }
-      await Future.delayed(Duration(milliseconds: 0xDD));
-      return true;
-    }).then((_) {
-      // Update the Address Field
-      FlutterBluetoothSerial.instance.address.then((address) {
-        setState(() {
-          _address = address;
-        });
-      });
-    });
-
-    FlutterBluetoothSerial.instance.name.then((name) {
-      setState(() {
-        _name = name;
-      });
-    });
-
-    // Listen for further state changes:
-    FlutterBluetoothSerial.instance
-        .onStateChanged()
-        .listen((BluetoothState state) {
-      _bluetoothState = state;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Home"),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.play_arrow),
-              onPressed: () async {
-                print('Hallo Welt!');
-                final BluetoothDevice selDevice = await Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (context) {
-                  return SelectBondedDevicePage(checkAvailability: false);
-                }));
-
-                if (selDevice != null) {
-                  print("Connected to device!");
-                  await _startMeasuring(context, selDevice);
-                  if (_collectingTask != null) {
-                    print("opening page route");
-                    await Navigator.of(context)
-                        .push(MaterialPageRoute(builder: (context) {
-                      return ScopedModel<BackgroundCollectingTask>(
-                        model: _collectingTask,
-                        child: MeasureDataPage(),
-                      );
-                    }));
-                  }
-                } else {
-                  print(
-                      'Error while bonding device, you may need to enable bluetooth!');
-                }
-              },
-            )
-          ],
-        ),
-        body: Center(
-          child: Text(
-              "Please press the play button to start a new measurement row"),
-        ));
-  }
-
-  Future<void> _startMeasuring(
-      BuildContext context, BluetoothDevice device) async {
-    try {
-      _collectingTask = await BackgroundCollectingTask.connect(device);
-      await _collectingTask.start();
-    } catch (ex) {
-      if (_collectingTask != null) {
-        _collectingTask.cancel();
-      }
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: const Text('Error occured while connecting!'),
-            content: Text('${ex.toString()}'),
-            actions: <Widget>[
-              new FlatButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: new Text('Close'))
-            ],
-          );
-        },
-      );
-    }
-  }
-
-  Future<void> _stopMeasuring() async {
-    if (_collectingTask != null) {
-      _collectingTask.cancel();
-    }
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem> [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            title: Text('Karte')
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bluetooth),
+            title: Text('Bluetooth Data'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            title: Text('Einstellungen'),
+          )
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
