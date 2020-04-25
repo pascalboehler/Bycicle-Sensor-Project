2a53341faecabedd6cf1576e0f49165af006f412
nucleus-ffm <nico-git@posteo.de>
2020-04-25 15:58:52 +0200

    added chart visualisation, not yet ready

--- app/digiroad/lib/MeasureDataPage.dart
+++ app/digiroad/lib/MeasureDataPage.dart
@@ -1,15 +1,95 @@
 import 'package:flutter/material.dart';
 import './BackgroundCollectingTask.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 
-class MeasureDataPage extends StatelessWidget {
+class MeasureDataPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    final BackgroundCollectingTask task =
-        BackgroundCollectingTask.of(context, rebuildOnChange: true);
+  _MeasureDataPageState createState() => _MeasureDataPageState();
+}
 
-    return Scaffold(
+class _MeasureDataPageState extends State<MeasureDataPage> {
+    //Data to displayed in the pieChart, yet example Data
+    double distance1 = 150;
+    double distance2 = 200; // TODO: insert true data from the sensor
+    double distance3 = 300;
+
+    double maxDistance = 300; // Max Distance to get a static relationship in the chart, maybe it has to change
+
+    Color distanceColor1 = Colors.green ;
+    Color distanceColor2 = Colors.green ; //TODO: change color if to close
+    Color distanceColor3 = Colors.green ;
+
+    List<charts.Series<DistanceDataPie, String>> _seriesPieData;
+    List<charts.Series<DistanceDataPie, String>> _seriesPieData2;
+    List<charts.Series<DistanceDataPie, String>> _seriesPieData3;
+
+    _generateData() {
+      var pieData = [
+        new DistanceDataPie('Distance1', distance1, distanceColor1),
+        new DistanceDataPie(
+            'rest', maxDistance - distance1, Colors.transparent),
+      ];
+      _seriesPieData.add(
+        charts.Series(
+          data: pieData,
+          domainFn: (DistanceDataPie task, _) => task.distance,
+          measureFn: (DistanceDataPie task, _) => task.distancevalue,
+          colorFn: (DistanceDataPie task, _) =>
+              charts.ColorUtil.fromDartColor(task.colorvalue),
+          id: 'distance-measure1',
+          labelAccessorFn: (DistanceDataPie row, _) => '${row.distancevalue}',
+        ),
+      );
+      var pieData2 = [
+        new DistanceDataPie('Distance2', distance2, distanceColor2),
+        new DistanceDataPie(
+            'rest', maxDistance - distance2, Colors.transparent),
+      ];
+      _seriesPieData2.add(
+        charts.Series(
+          data: pieData2,
+          domainFn: (DistanceDataPie task, _) => task.distance,
+          measureFn: (DistanceDataPie task, _) => task.distancevalue,
+          colorFn: (DistanceDataPie task, _) =>
+              charts.ColorUtil.fromDartColor(task.colorvalue),
+          id: 'distance-measure2',
+          labelAccessorFn: (DistanceDataPie row, _) => '${row.distancevalue}',
+        ),
+      );
+      var pieData3 = [
+        new DistanceDataPie('Distance3', distance3, distanceColor3),
+        new DistanceDataPie(
+            'rest', maxDistance - distance3, Colors.transparent),
+      ];
+      _seriesPieData3.add(
+        charts.Series(
+          data: pieData3,
+          domainFn: (DistanceDataPie task, _) => task.distance,
+          measureFn: (DistanceDataPie task, _) => task.distancevalue,
+          colorFn: (DistanceDataPie task, _) =>
+              charts.ColorUtil.fromDartColor(task.colorvalue),
+          id: 'distance-measure3',
+          labelAccessorFn: (DistanceDataPie row, _) => '${row.distancevalue}',
+        ),
+      );
+    }
+  @override
+  void initState(){
+    super.initState();
+    _seriesPieData =  List<charts.Series<DistanceDataPie, String>>();
+    _seriesPieData2 = List<charts.Series<DistanceDataPie, String>>();
+    _seriesPieData3 = List<charts.Series<DistanceDataPie, String>>();
+    _generateData();
+  }
+
+    @override
+    Widget build(BuildContext context) {
+      final BackgroundCollectingTask task =
+      BackgroundCollectingTask.of(context, rebuildOnChange: true);
+
+      return Scaffold(
       appBar: AppBar(
-        title: Text("Home"),
+        title: Text("MeasureDataPage"),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.pause),
@@ -19,30 +99,82 @@ class MeasureDataPage extends StatelessWidget {
           )
         ],
       ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Text(
-                    'Sensor 1: ${task != null && task.dataList.length != 0 ? task.dataList.last.distance1 : "NULL"}'),
-              ],
+      body: Padding(
+        padding: const EdgeInsets.all(2.0),
+        child: Container(
+          child: Center(
+            child: Column(
+                children: <Widget>[
+                  Text('left'),
+                  Text(
+                      'Sensor 1: ${task != null && task.dataList.length != 0 ? task.dataList.last.distance1 : "NULL"}'),
+                  SizedBox(height: 1.0),
+                  Expanded(
+                    child: charts.PieChart(
+                      _seriesPieData,
+                      animate: true,
+                      animationDuration: Duration(seconds: 0), //TODO: test different animationDuration
+                      defaultRenderer: new charts.ArcRendererConfig(
+                          arcWidth: 50,
+                          arcRendererDecorators: [
+                            new charts.ArcLabelDecorator(
+                                labelPosition: charts.ArcLabelPosition.inside
+                            )
+                          ]
+                      ),
+                    ),
+                  ),
+                  Text('right'),
+                  Text(
+                      'Sensor 2: ${task != null && task.dataList.length != 0 ? task.dataList.last.distance2 : "NULL"}'),
+                  SizedBox(height: 1.0),
+                  Expanded(
+                    child: charts.PieChart(
+                      _seriesPieData2,
+                      animate: true,
+                      animationDuration: Duration(seconds: 0), //TODO: test different animationDuration
+                      defaultRenderer: new charts.ArcRendererConfig(
+                          arcWidth: 50,
+                          arcRendererDecorators: [
+                            new charts.ArcLabelDecorator(
+                                labelPosition: charts.ArcLabelPosition.inside
+                            ),
+                          ],
+                      ),
+                    ),
+                  ),
+                  Text('behind'),
+                  Text(
+                      'Sensor 3: ${task != null && task.dataList.length != 0 ? task.dataList.last.distance3 : "NULL"}'),
+                  SizedBox(height: 1.0),
+                  Expanded(
+                    child: charts.PieChart(
+                      _seriesPieData3,
+                      animate: true,
+                      animationDuration: Duration(seconds: 0), //TODO: test different animationDuration
+                      defaultRenderer: new charts.ArcRendererConfig(
+                          arcWidth: 50,
+                          arcRendererDecorators: [
+                            new charts.ArcLabelDecorator(
+                                labelPosition: charts.ArcLabelPosition.inside
+                            )
+                          ],
+                      ),
+                    ),
+                  ),
+                ],
             ),
-            Row(
-              children: <Widget>[
-                Text(
-                    'Sensor 2: ${task != null && task.dataList.length != 0 ? task.dataList.last.distance2 : "NULL"}'),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                Text(
-                    'Sensor 3: ${task != null && task.dataList.length != 0 ? task.dataList.last.distance3 : "NULL"}'),
-              ],
-            )
-          ],
+          ),
         ),
       ),
     );
   }
+}
+
+class DistanceDataPie{
+  String distance;
+  double distancevalue;
+  Color colorvalue;
+
+  DistanceDataPie(this.distance, this.distancevalue, this.colorvalue);
 }
